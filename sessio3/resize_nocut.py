c0103759851571857067e188d7b72b72b7e969be
JoanGDSA <joan.montserrat@estudiant.upc.edu>
2017-11-22 23:54:15 +0100

    Updated comments for clarity.
    Final commit.
    Take into account, that the scripts unique purpouse is to resize the image to maximum dimension of 500. Therefore, no cropping is performed to impose a given aspect ratio. That is the main difference with the previous script: resize.py

--- sessio3/resize_nocut.py
+++ sessio3/resize_nocut.py
@@ -2,18 +2,21 @@
 import os
 from scipy import misc
 
+# Specifies which is the largest size on any side of the picture. (Caters for portrait and landscape)
 FIXED_MAX_DIMENSION = 500.0
 
+# For each allocated class, save pictures in their containing class folder.
 for class_ in ['desconegut', 'mercat_independencia', 'mnactec']:
     path = '../../pictures/' + class_ + '/'
     savedir = '../../resized/' + class_ + '/'
 
+    # Create a list describing only the files inside a the specified path. (Omits filesystem folders and files)
     flist = []
-    for (dirpath, dirnames, filenames) in os.walk(path):
-        flist.extend(filenames)
+    for (dirpath, dirnames, filenames) in os.walk(path):    # Crawl through directories,
+        flist.extend(filenames)                             # to find the full file paths.
         break
 
-    for filename in flist:
-        pic = misc.imread(path + filename)
-        pic_resized = misc.imresize(pic, FIXED_MAX_DIMENSION / max(pic.shape), interp='nearest', mode=None)
-        misc.imsave(savedir + filename, pic_resized)
+    for filename in flist:                                  # With each file's name,
+        pic = misc.imread(path + filename)                  # decode the file contents,
+        pic_resized = misc.imresize(pic, FIXED_MAX_DIMENSION / max(pic.shape), interp='nearest', mode=None) # scale,
+        misc.imsave(savedir + filename, pic_resized)        # and save into a new folder.
